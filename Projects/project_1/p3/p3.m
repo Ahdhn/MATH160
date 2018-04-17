6c7af4a6de780bc99d46859da8f2de9f45fb8923
Ahmed <eng.a.hassen@gmail.com>
2018-04-17 14:17:42 -0700

    p3 - colley's method

--- Projects/project_1/p3/p3.m
+++ Projects/project_1/p3/p3.m
@@ -18,9 +18,12 @@ exam_mat = importdata('examscores.dat',',');
 %     ];
 %compute the number of student and exams 
 [num_student, num_exams] = size(exam_mat); 
+
+%%%%%%%%%%%%%%% Massey Method
 %get massey matrix (last row is all-ones)
 massey_mat = (-num_student).*ones(num_exams);
 massey_mat(logical(eye(num_exams)))=((num_exams-1)*num_student);
+colley_mat = massey_mat + 2.0.*eye(num_exams);
 
 %compute the matrix of score differentials 
 diff_mat = zeros(num_student, num_exams);
@@ -48,17 +51,73 @@ diff_vector(end) = 0;
 %solve the equations 
 y = massey_mat\diff_vector;
 
-%sort the rating from hardest to easiet
-[sorted_y, sorting_id] = sort(y);
+%sort the rating from hardest to easiest
+display_rating(1,y);
 
-disp(['Using Massey network method, below is the questions', char(10),...
-    '   sorted from most difficult to easier (with rating): ']);
-for k=1:num_exams
-    for l=1:num_exams
-        if sorting_id(l)== k
-            break;
+
+%%%%%%%%%%%%%%% Colley Method
+%compute colley b vector
+win = zeros(num_student,num_exams);
+loss = zeros(num_student,num_exams);
+for id_i = 1:num_exams
+    for id_j = 1:num_exams   
+        if id_i == id_j 
+            continue;
         end
+        win(:,id_i) = win(:,id_i) +...
+            double(exam_mat(:,id_i) > exam_mat(:,id_j))+ ...
+            +0.5*double(exam_mat(:,id_i) == exam_mat(:,id_j));
+        
+        loss(:,id_i) = loss(:,id_i) +...
+            double(exam_mat(:,id_i) < exam_mat(:,id_j)) + ...
+            +0.5*double(exam_mat(:,id_i) == exam_mat(:,id_j));
     end
-    disp([' Q. ',num2str(l) , ' (',num2str(y(l)) ,')' ]);
 end
 
+
+%check each row sum to zero
+for id_i = 1:num_exams
+    if sum(win(:,id_i))+sum(loss(:,id_i)) ~= (num_exams-1)*num_student
+        warning(['row', num2str(id_i) ,'in mat_w and mat_l does not sum up right']);
+    end
+end
+
+win  = sum(win,1);
+loss = sum(loss,1);
+
+%compute b vector (the RHS of colley system of equations)
+b = zeros(num_exams,1);
+for id_i = 1:num_exams
+    b(id_i) = 1.0 + ((win(id_i)-loss(id_i))/2.0);
+end  
+
+C = colley_mat\b;
+display_rating(2,C);
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function display_rating(method, rating_vect)
+    
+    %first sort the rating from hardest to easiest
+    [sorted_y, sorting_id] = sort(rating_vect);
+    if method ==1
+        disp(['Using Massey network method, below is the questions', char(10),...
+        '   sorted from most difficult to easier (with rating): ']);        
+    else
+        disp(['Using Colley method, below is the questions', char(10),...
+        '   sorted from most difficult to easier (with rating): ']);
+    end
+    
+    num_exams = length(rating_vect);
+    
+    for k=1:num_exams
+        for l=1:num_exams
+            if sorting_id(l)== k
+                break;
+            end
+        end
+        disp([' Q. ',num2str(l) , ' (',num2str(rating_vect(l)) ,')' ]);
+    end
+end
