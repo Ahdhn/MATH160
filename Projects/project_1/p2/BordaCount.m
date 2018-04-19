2bce85e5a36a778c26eff31ed802b10dcad8ac6c
Ahmed <eng.a.hassen@gmail.com>
2018-04-18 20:07:31 -0700

    borda bug fix  + w-borda

--- Projects/project_1/p2/BordaCount.m
+++ Projects/project_1/p2/BordaCount.m
@@ -25,9 +25,9 @@ for n=1:num_candidate
             (num_candidate-m+1)*(rankingcandidates(:,m) == candidate_string);
     end
 end
-borda_score_mat = zeros(num_voter, num_candidate);
 
-for m=2:num_candidate
+borda_score_mat = zeros(num_voter, num_candidate);
+for m=1:num_candidate
     %not that in position_mat the order is reversed
     %first position gets 5 and second position get 4
     %this affect the borda weight
@@ -78,46 +78,4 @@ function candidate_string = getCandidateName(candidate_num)
         warning('Invalid input to getNum()');
     end
 end
-%{
-total_DT = 0;
-total_BS = 0;
-total_TC = 0;
-total_JK = 0;
-total_HC = 0;
 
-for n = 1:5
-    DT = count(rankingcandidates(:,n), "DT");
-    score_DT = sum(DT,1)*(5-n);
-    total_DT = total_DT + score_DT;
-end
-
-for n = 1:5
-    BS = count(rankingcandidates(:,n), "BS");
-    score_BS = sum(BS,1)*(5-n);
-    total_BS = total_BS + score_BS;
-end
-
-for n = 1:5
-    TC = count(rankingcandidates(:,n), "TC");
-    score_TC = sum(TC,1)*(5-n);
-    total_TC = total_TC + score_TC;
-end
-
-for n = 1:5
-    JK = count(rankingcandidates(:,n), "JK");
-    score_JK = sum(JK,1)*(5-n);
-    total_JK = total_JK + score_JK;
-end
-
-for n = 1:5
-    HC = count(rankingcandidates(:,n), "HC");
-    score_HC = sum(HC,1)*(5-n);
-    total_HC = total_HC + score_HC;
-end
-
-fprintf('Donald Trump has a Borda score of %d.\n',total_DT)
-fprintf('Bernie Sanders has a Borda score of %d.\n',total_BS)
-fprintf('Ted Cruz has a Borda score of %d.\n',total_TC)
-fprintf('John Kasich has a Borda score of %d.\n',total_JK)
-fprintf('Hilary Clinton has a Borda score of %d.\n',total_HC)
-%}
